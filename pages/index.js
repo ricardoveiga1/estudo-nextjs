30b3f78e9fe35630d46832badf82df0d8ce5c650
Ricardo Veiga <ricardoveiga.ti@gmail.com>
2021-09-09 18:12:08 -0300

    primeira pagina

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
+
 function Home(){
-    return <div> Página Inicial</div>
+    return (<div>
+        <h1> Home</h1>
+    <a href="/sobre">Acessar página sobre</a>
+    </div>)
 }
 
 export default Home
---
+++ pages/sobre.js
@@ -0,0 +1,9 @@
+
+function Sobre(){
+    return (<div>
+        <h1>Sobre</h1>
+    <a href="/">Acessar página home</a>
+    </div>)
+}
+
+export default Sobre
